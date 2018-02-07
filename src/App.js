c7a0a308fa481b2eda5f8ee014dca8fa16216689
chayeoi <y2oniary@gmail.com>
2018-02-07 19:19:54 +0900

    코드 변경 - src/App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div>
+        <header>
+          <h1>강의 리뷰 플랫폼, 댓댓</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
