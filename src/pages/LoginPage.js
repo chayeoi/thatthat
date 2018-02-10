1b6d2e3a2450061ddec780966efcbdc874a574a1
chayeoi <y2oniary@gmail.com>
2018-02-10 18:56:07 +0900

    페이지 추가 - 로그인 페이지

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Container } from 'semantic-ui-react'
+
+export default class LoginPage extends Component {
+  render() {
+    return (
+      <Container>
+        <h1>Hello, ThatThat.</h1>
+      </Container>
+    )
+  }
+}
