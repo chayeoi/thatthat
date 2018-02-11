590cef901ed41edcbaab8f13b7b5f3c2dbc60555
chayeoi <y2oniary@gmail.com>
2018-02-12 00:23:42 +0900

    프로젝트 설정 초기화 - React Router 및 Redux 초기 세팅

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunkMiddleware from 'redux-thunk'
+
+import reducer from 'ducks'
+import { LoginPage } from 'pages'
+
+
+const store = createStore(
+  reducer,
+  applyMiddleware(thunkMiddleware),
+)
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <header>
-          <h1>강의 리뷰 플랫폼, 댓댓</h1>
-        </header>
-      </div>
+      <Provider store={store}>
+        <Router>
+          <div>
+            <Route path="/login" component={LoginPage} />
+          </div>
+        </Router>
+      </Provider>
     )
   }
 }
