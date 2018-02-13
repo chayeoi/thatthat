71d6f450d3e5445098b7ef3ee545aee5878ba85e
chayeoi <y2oniary@gmail.com>
2018-02-14 00:33:19 +0900

    개발 도구 추가 - Redux Devtools Extension

--- src/ducks/index.js
+++ src/ducks/index.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import * as modules from './modules'
 
@@ -6,7 +6,9 @@ const reducer = combineReducers({
   ...modules,
 })
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
 export default createStore(
   reducer,
-  applyMiddleware(thunkMiddleware),
+  composeEnhancers(applyMiddleware(thunkMiddleware)),
 )
