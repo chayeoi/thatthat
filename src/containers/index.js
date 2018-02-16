d653800d32f58db759df7587067735747d988924
chayeoi <y2oniary@gmail.com>
2018-02-16 17:30:57 +0900

    디렉토리 구조 변경 - App.js를 /containers로 이동

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,8 +1,10 @@
+import App from './App'
 import LoginButtonContainer from './LoginButtonContainer'
 import CategoryListContainer from './CategoryListContainer'
 import CourseListContainer from './CourseListContainer'
 
 export {
+  App,
   LoginButtonContainer,
   CategoryListContainer,
   CourseListContainer,
