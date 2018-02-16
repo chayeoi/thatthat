d5adde54eb7919a06b205ac3c1154c61bc756ccb
chayeoi <y2oniary@gmail.com>
2018-02-16 12:34:57 +0900

    기능 추가 - 카테고리 별 데이터 불러오기

--- src/containers/CourseListContainer.js
+++ src/containers/CourseListContainer.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import { CourseList } from 'components'
+import { loadCourseList } from 'ducks/modules/category'
 
-export default class CourseListContainer extends Component {
+class CourseListContainer extends Component {
+  static defaultProps = {
+    courses: [],
+    onMount: () => {},
+  }
+  // XXX: 특정 카테고리를 클릭했을 때 각 카테고리에 맞는 새 데이터를 불러오지 못하고 있다.
+  componentDidMount() {
+    this.props.onMount()
+  }
+
   render() {
-    const { category } = this.props.match.params
+    const { courses } = this.props
     return (
       <div>
-        <h1>{category}</h1>
-        <CourseList />
+        {courses.map(course => (
+          <CourseList key={course.id} course={course} />
+        ))}
       </div>
     )
   }
 }
+
+export default connect(
+  state => ({
+    courses: state.category.courses,
+  }),
+  (dispatch, ownProps) => {
+    const { category } = ownProps.match.params || ''
+    return {
+      onMount: () => dispatch(loadCourseList(category)),
+    }
+  },
+)(CourseListContainer)
