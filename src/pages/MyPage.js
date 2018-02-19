767189ddbdd7375c3dbbd42cc05682229ad2b883
bong kim <32095134+underbleu@users.noreply.github.com>
2018-02-19 16:12:39 +0900

    기능 추가 - 마이페이지 생성

---
+++ src/pages/MyPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { MainMenu } from 'components'
+
+const MyPage = () => (
+  <React.Fragment>
+    <MainMenu />
+  마이페이지
+  </React.Fragment>
+)
+
+export default MyPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import MainPage from './MainPage'
 import LoginPage from './LoginPage'
 import CoursePage from './CoursePage'
+import MyPage from './MyPage'
 
 export {
   MainPage,
   LoginPage,
   CoursePage,
+  MyPage,
 }
