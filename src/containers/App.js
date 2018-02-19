d240aa0cbf305c2f2a9462c433acc52c91545674
chayeoi <y2oniary@gmail.com>
2018-02-19 18:59:19 +0900

    페이지 추가 - NotFoundPage

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,8 +5,13 @@ import {
   Switch,
 } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { LoginPage, MainPage, MyPage } from 'pages'
 import store from 'ducks'
+import {
+  LoginPage,
+  MainPage,
+  MyPage,
+  NotFoundPage,
+} from 'pages'
 
 class App extends Component {
   render() {
@@ -14,9 +19,10 @@ class App extends Component {
       <Provider store={store}>
         <Router>
           <Switch>
+            <Route exact path="/" component={MainPage} />
             <Route path="/login" component={LoginPage} />
             <Route path="/mypage" component={MyPage} />
-            <Route path="/" component={MainPage} />
+            <Route component={NotFoundPage} />
           </Switch>
         </Router>
       </Provider>
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import {
   CategoryListContainer,
   CourseListContainer,
@@ -10,8 +10,10 @@ const MainPage = () => (
   <React.Fragment>
     <MainMenu />
     <CategoryListContainer />
-    <Route exact path="/" component={CourseListContainer} />
-    <Route path="/courses/:category" component={CourseListContainer} />
+    <Switch>
+      <Route exact path="/" component={CourseListContainer} />
+      <Route path="/courses/:category" component={CourseListContainer} />
+    </Switch>
   </React.Fragment>
 )
 
