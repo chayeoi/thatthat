577339cb735817e42f997796038838acbf24c8cd
bong kim <32095134+underbleu@users.noreply.github.com>
2018-03-01 03:01:05 +0900

    Merge

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -9,18 +9,35 @@ import {
   MyReviewListContainer,
 } from 'containers'
 import { withAuth } from 'hocs'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  height: 100vh;
+  overflow: hidden;
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    z-index: -1;
+    width: 100%;
+    height: 100%;
+    background-size: cover;
+  }
+`
 
 const MyPage = () => (
   <React.Fragment>
-    <MainMenu />
-    <UserInfoContainer />
-    <LogoutButtonContainer />
-    <MyPageTabContainer />
-    <Switch>
-      <Route exact path="/mypage" render={() => <Redirect to="/mypage/likes" />} />
-      <Route path="/mypage/likes" component={MyLikeListContainer} />
-      <Route path="/mypage/reviews" component={MyReviewListContainer} />
-    </Switch>
+    <Wrapper>
+      <MainMenu />
+      <UserInfoContainer />
+      <LogoutButtonContainer />
+      <MyPageTabContainer />
+      <Switch>
+        <Route exact path="/mypage" render={() => <Redirect to="/mypage/likes" />} />
+        <Route path="/mypage/likes" component={MyLikeListContainer} />
+        <Route path="/mypage/reviews" component={MyReviewListContainer} />
+      </Switch>
+    </Wrapper>
   </React.Fragment>
 )
 
