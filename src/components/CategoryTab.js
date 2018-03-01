c813af1474aa7113eb91bf1325c485243865a44e
bong kim <32095134+underbleu@users.noreply.github.com>
2018-03-02 06:14:15 +0900

    스타일링 - CategoryTab, CourseList, MainMenu 모바일버전 스타일링 완료

--- src/components/CategoryTab.js
+++ src/components/CategoryTab.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
 import { Menu } from 'semantic-ui-react'
+import * as color from '../constants/color'
+
+const CategoryItem = styled(Menu.Item)`
+  font-weight: 600 !important;
+  color: ${color.GRAY6} !important;
+`
 
 export default class CategoryList extends Component {
   state = {
@@ -15,9 +22,9 @@ export default class CategoryList extends Component {
     const { categories } = this.props
     return (
       <div>
-        <Menu pointing secondary color="red">
+        <Menu pointing secondary color="red" widths={5}>
           {categories.map(category => (
-            <Menu.Item
+            <CategoryItem
               key={category.name}
               name={category.name}
               active={activeItem === category.link.to}
--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,27 +1,32 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Menu, Icon } from 'semantic-ui-react'
-import { MAIN_COLOR } from 'constants/color'
+import { MAIN_COLOR } from '../constants/color'
 
-const Wrapper = styled.div`
-  text-align: center;
+const Wrapper = styled.header`
+  /* height: 44px; */
   background-color: ${MAIN_COLOR};
 `
+const Logo = styled.h1`
+  font-family: 'Spoqa Han Sans', sans-serif;
+  font-weight: 900;
+  font-size: 22px;
+`
+const MenuBox = styled(Menu)`
+  height: 44px !important;
+  background-color: transparent !important;
+`
+
 const MainMenu = ({ links }) => (
   <Wrapper>
-    <Menu secondary inverted>
-      <Menu.Item
-        name="댓댓"
-        {...links.home}
-      />
-      <Menu.Item
-        name="messages"
-        position="right"
-        {...links.myPage}
-      >
-        <Icon name="user" />
+    <MenuBox inverted>
+      <Menu.Item {...links.home}>
+        <Logo>댓댓</Logo>
       </Menu.Item>
-    </Menu>
+      <Menu.Item {...links.myPage} position="right" >
+        <Icon className="icon-user-outline" size="large" />
+      </Menu.Item>
+    </MenuBox>
   </Wrapper>
 )
 
