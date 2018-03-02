eb78ad81eaafc024605c8179a84304945fc507d1
bong kim <32095134+underbleu@users.noreply.github.com>
2018-03-02 17:01:18 +0900

    스타일링 - fontello폰트 적용방식 변경후, MyPage 메뉴 아이콘 추가

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -4,17 +4,20 @@ import { Menu, Icon } from 'semantic-ui-react'
 import { MAIN_COLOR } from '../constants/color'
 
 const Wrapper = styled.header`
-  /* height: 44px; */
   background-color: ${MAIN_COLOR};
 `
+const MenuBox = styled(Menu)`
+  height: 44px !important;
+  background-color: transparent !important;
+  `
 const Logo = styled.h1`
   font-family: 'Spoqa Han Sans', sans-serif;
   font-weight: 900;
   font-size: 22px;
 `
-const MenuBox = styled(Menu)`
-  height: 44px !important;
-  background-color: transparent !important;
+const MypageIcon = styled.i`
+  font-size: 19px;
+  font-family: fontello;
 `
 
 const MainMenu = ({ links }) => (
@@ -24,7 +27,7 @@ const MainMenu = ({ links }) => (
         <Logo>댓댓</Logo>
       </Menu.Item>
       <Menu.Item {...links.myPage} position="right" >
-        <Icon className="icon-user-outline" size="large" />
+        <MypageIcon className="icon-user-outline" />
       </Menu.Item>
     </MenuBox>
   </Wrapper>
