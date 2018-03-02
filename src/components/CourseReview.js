5a5397325398d71311cf726f8bcd99b18829fea3
chayeoi <y2oniary@gmail.com>
2018-03-02 21:14:31 +0900

    기능 개선 - 등록된 리뷰가 없을 시 메세지 표시하기

--- src/components/CourseReview.js
+++ src/components/CourseReview.js
@@ -3,9 +3,12 @@ import { ReviewCard } from 'components'
 
 const CourseReview = ({ reviews }) => (
   <ul>
-    {reviews.map(({ reviewKey, ...rest }) => (
-      <ReviewCard key={reviewKey} {...rest} />
-    ))}
+    {reviews ?
+      reviews.map(({ reviewKey, ...rest }) => (
+        <ReviewCard key={reviewKey} {...rest} />
+      ))
+      : <div>등록된 리뷰가 없습니다.</div>
+    }
   </ul>
 )
 
