498b1ba961fb535bb165570e4f604c00bb7a5f4d
bong kim <32095134+underbleu@users.noreply.github.com>
2018-03-04 21:36:15 +0900

    스타일링 - 리뷰카드 디자인

--- src/components/ReviewCard.js
+++ src/components/ReviewCard.js
@@ -1,4 +1,28 @@
 import React from 'react'
+import { Segment, Comment, Rating } from 'semantic-ui-react'
+import styled from 'styled-components'
+
+const CommentBox = styled(Comment.Group)`
+  max-width: 100% !important;
+`
+
+const UserName = styled(Comment.Author)`
+  display: inline-block !important;
+`
+
+const CommentText = styled(Comment.Text)`
+  margin-top: 2px !important;
+  margin-bottom: 0 !important;
+`
+
+const StyledRating = styled(Rating)`
+  display: block !important;
+  margin-top: 5px;
+  font-size: .7rem !important;
+  &.active::before {
+    color: #f8ba00;
+  }
+`
 
 const ReviewCard = ({
   reviewKey,
@@ -10,14 +34,20 @@ const ReviewCard = ({
   uid,
   render,
 }) => (
-  <li>
-    <div>{content}</div>
-    <div>{createdAt}</div>
-    <div>{displayName}</div>
-    <img src={photoURL} alt="유저 사진" />
-    <div>{rating}</div>
-    {render(uid, reviewKey, rating)}
-  </li>
+  <Segment as="li">
+    <CommentBox>
+      <Comment>
+        <Comment.Avatar src={photoURL} alt={`${displayName} 프로필 사진`} />
+        <Comment.Content>
+          <UserName>{displayName}</UserName>
+          <Comment.Metadata>{createdAt}</Comment.Metadata>
+          <StyledRating defaultRating={rating} maxRating={5} disabled />
+          <CommentText>{content}</CommentText>
+        </Comment.Content>
+        {render(uid, reviewKey, rating)}
+      </Comment>
+    </CommentBox>
+  </Segment>
 )
 
 export default ReviewCard
