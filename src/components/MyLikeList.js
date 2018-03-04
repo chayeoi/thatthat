73bac2b604a00fad71a0bf0150a5434eddd57456
bong kim <32095134+underbleu@users.noreply.github.com>
2018-03-04 14:08:44 +0900

    스타일링 - 마이페이지의 기본배경 사이즈 화면에 꽉차도록 Wrapper 크기 조정

--- src/components/MyLikeList.js
+++ src/components/MyLikeList.js
@@ -1,15 +1,25 @@
 import React from 'react'
+import styled from 'styled-components'
 import { CourseCard } from 'components'
+import * as color from '../constants/color'
+
+const Wrapper = styled.ul`
+  padding: 1em;
+  min-height: 100vh;
+  background-color: ${color.GRAY2};
+`
 
 const MyLikeList = ({ likes }) => (
-  <ul>
-    {likes ?
-      likes.map(course => (
-        <CourseCard key={course.courseKey} course={course} />
-      ))
-      : <div>좋아요 누른 강의가 없습니다.</div>
-    }
-  </ul>
+  <Wrapper>
+    <ul>
+      {likes ?
+        likes.map(course => (
+          <CourseCard key={course.courseKey} course={course} />
+        ))
+        : <div>좋아요 누른 강의가 없습니다.</div>
+      }
+    </ul>
+  </Wrapper>
 )
 
 export default MyLikeList
