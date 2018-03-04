c39749e937095a1c12c932c2b6aa5249bb65858d
bong kim <32095134+underbleu@users.noreply.github.com>
2018-03-04 16:21:16 +0900

    스타일링 - 코스 정보 기본배경 사이즈 화면에 꽉차도록 Wrapper 크기 조정

--- src/components/CourseInfo.js
+++ src/components/CourseInfo.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import styled from 'styled-components'
+import * as color from '../constants/color'
+
+const Wrapper = styled.ul`
+  padding: 1em;
+  min-height: 100vh;
+  background-color: ${color.GRAY2};
+`
 
 const CourseInfo = ({ content }) => (
-  <div>
+  <Wrapper>
     {content}
-  </div>
+  </Wrapper>
 )
 
 export default CourseInfo
