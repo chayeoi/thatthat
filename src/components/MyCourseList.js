543d103d94deb018ea8e0b98e1aed815adbe96cd
bong kim <32095134+underbleu@users.noreply.github.com>
2018-03-06 01:44:41 +0900

    스타일링 - 좋아요 강의 목록 디자인

--- src/components/MyCourseList.js
+++ src/components/MyCourseList.js
@@ -1,15 +1,23 @@
 import React from 'react'
+import styled from 'styled-components'
 import { CourseCard } from 'components'
+import { GRAY2 } from '../constants/color'
+
+const Wrapper = styled.ul`
+  padding: 1rem 1rem;
+  min-height: 100vh;
+  background-color: ${GRAY2};
+`
 
 const MyCourseList = ({ courses }) => (
-  <ul>
+  <Wrapper>
     {courses ?
       courses.map(course => (
         <CourseCard key={course.courseKey} course={course} />
       ))
       : <div>등록한 강의가 없습니다.</div>
     }
-  </ul>
+  </Wrapper>
 )
 
 export default MyCourseList
