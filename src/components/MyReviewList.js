f11d89a7dfcc8e6f05ea83005debc1f181eb829f
chayeoi <y2oniary@gmail.com>
2018-03-05 19:30:40 +0900

    기능 추가 - 내가 등록한 리뷰 갯수 표시

--- src/components/MyReviewList.js
+++ src/components/MyReviewList.js
@@ -2,14 +2,17 @@ import React from 'react'
 import { MyReviewCard } from 'components'
 
 const MyReviewList = ({ reviews }) => (
-  <ul>
-    {reviews ?
-      reviews.map(review => (
-        <MyReviewCard key={review.reviewKey} review={review} />
-      ))
-      : <div>등록한 리뷰가 없습니다.</div>
-    }
-  </ul>
+  <React.Fragment>
+    {reviews.length ? <div>총 {reviews.length}개의 리뷰가 있습니다.</div> : null}
+    <ul>
+      {reviews ?
+        reviews.map(review => (
+          <MyReviewCard key={review.reviewKey} review={review} />
+        ))
+        : <div>등록한 리뷰가 없습니다.</div>
+      }
+    </ul>
+  </React.Fragment>
 )
 
 export default MyReviewList
