c1d731b02ac24117890e0c754053bf28f04ff1e7
chayeoi <y2oniary@gmail.com>
2018-03-05 21:10:24 +0900

    기능 추가 - 내가 좋아요 누른 강의에 좋아요 표시할 데이터 가져오기

--- src/utils/processCourseData.js
+++ src/utils/processCourseData.js
@@ -1,8 +1,23 @@
 import * as firebase from 'firebase'
 
 const processCourseData = async (rawCourses) => {
+  const { uid } = firebase.auth().currentUser
   const pendingCourses = Object.entries(rawCourses).map(async ([courseKey, course]) => {
-    const recentReviewInfoSnapshot = await firebase.database().ref(`reviewInfo/${courseKey}`).limitToLast(1).once('value')
+    const recentReviewInfoPromise = firebase.database().ref(`reviewInfo/${courseKey}`).limitToLast(1).once('value')
+    const myLikePromise = firebase.database().ref(`myLikes/${uid}/${courseKey}`).once('value')
+    const [
+      recentReviewInfoSnapshot,
+      myLikeSnapshot,
+    ] = await Promise.all([
+      recentReviewInfoPromise,
+      myLikePromise,
+    ])
+    let myLike
+    if (myLikeSnapshot.val()) {
+      myLike = 1
+    } else {
+      myLike = 0
+    }
     const recentReviewInfo = recentReviewInfoSnapshot.val()
     let recentReview = null
     if (recentReviewInfo) {
@@ -25,6 +40,7 @@ const processCourseData = async (rawCourses) => {
       courseKey,
       recentReview,
       ...course,
+      myLike,
     }
   })
   const courses = await Promise.all(pendingCourses)
