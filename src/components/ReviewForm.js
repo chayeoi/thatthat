ef64df88f5d96f27810c0f295caff938b745dc73
bong kim <32095134+underbleu@users.noreply.github.com>
2018-03-06 01:47:34 +0900

    스타일링 - 리뷰작성 input 디자인

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
-import { Form, TextArea, Rating } from 'semantic-ui-react'
+import { Form, TextArea, Rating, Icon } from 'semantic-ui-react'
+import * as color from '../constants/color'
 
 const Wrapper = styled.div`
   position: fixed;
   left: 0;
   bottom: 0;
-  background-color: white;
   width: 100%;
+  padding: 10px 1rem;
+  box-shadow: 0 -5px 10px -2px rgba(0,0,0,.12);
+  background-color: white;
 `
 
 // const StyledRating = styled(Rating)`
@@ -18,7 +21,31 @@ const Wrapper = styled.div`
 // `
 
 const MaxHeightTextArea = styled(TextArea)`
+  display: inline-block;
+  width: calc(100% - 30px) !important;
   max-height: 75px;
+  padding: .7rem 1.1rem;
+  border: none !important;
+  border-radius: 1.5rem !important;
+  background-color: ${color.GRAY1} !important;
+  caret-color: ${color.MAIN_COLOR};
+`
+
+const FormButton = styled.button`
+  position: absolute;
+  bottom: 10px;
+  right: 0;
+  display: inline-block;
+  width: 30px;
+  margin: 0;
+  color: #ced4da;
+  font-size: 1.2rem;
+  font-weight: normal;
+  text-align: right;
+  background-color: white;
+  &:hover {
+    color: ${color.MAIN_COLOR};
+  }
 `
 
 export default class ReviewForm extends Component {
@@ -55,24 +82,23 @@ export default class ReviewForm extends Component {
     return (
       <Wrapper>
         <Form loading={isCreating}>
-          <StyledRating icon="star" rating={rating} maxRating={5} onRate={this.handleRate} />
-          <label htmlFor="comment">
-            <MaxHeightTextArea
-              id="comment"
-              autoHeight
-              rows={1}
-              value={content}
-              placeholder="강의리뷰를 입력하세요..."
-              onChange={this.handleChange}
-            />
-          </label>
-          <Form.Button onClick={this.handleClick}>등록</Form.Button>
           {/* <StyledRating
             icon="star"
             rating={rating}
             maxRating={5}
             onRate={this.handleRate}
           /> */}
+          <label htmlFor="comment" className="readable-hidden" >리뷰작성</label>
+          <MaxHeightTextArea
+            id="comment"
+            rows={1}
+            value={content}
+            placeholder="강의리뷰를 입력하세요..."
+            onChange={this.handleChange}
+            autoHeight
+            required
+          />
+          <FormButton onClick={this.handleClick}><Icon name="send outline" /></FormButton>
         </Form>
       </Wrapper>
     )
