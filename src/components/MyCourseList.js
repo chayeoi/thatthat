004566ab906f3a89d3f9af86d6d9c071e30fd72a
chayeoi <y2oniary@gmail.com>
2018-03-05 19:31:58 +0900

    기능 추가 - 내가 등록한 강의 갯수 표시

--- src/components/MyCourseList.js
+++ src/components/MyCourseList.js
@@ -2,14 +2,17 @@ import React from 'react'
 import { CourseCard } from 'components'
 
 const MyCourseList = ({ courses }) => (
-  <ul>
-    {courses ?
-      courses.map(course => (
-        <CourseCard key={course.courseKey} course={course} />
-      ))
-      : <div>등록한 강의가 없습니다.</div>
-    }
-  </ul>
+  <React.Fragment>
+    {courses.length ? <div>총 {courses.length}개의 등록한 강의가 있습니다.</div> : null}
+    <ul>
+      {courses ?
+        courses.map(course => (
+          <CourseCard key={course.courseKey} course={course} />
+        ))
+        : <div>등록한 강의가 없습니다.</div>
+      }
+    </ul>
+  </React.Fragment>
 )
 
 export default MyCourseList
