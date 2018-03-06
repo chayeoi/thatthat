458cfca210cc3e9a37ba3978762f605d7a2b52fe
bong kim <32095134+underbleu@users.noreply.github.com>
2018-03-07 02:14:05 +0900

    스타일링 - 교육기관 Label 디자인

--- src/components/AcademyLabel.js
+++ src/components/AcademyLabel.js
@@ -4,26 +4,17 @@ import { Label } from 'semantic-ui-react'
 import { SUB_COLOR } from 'constants/color'
 
 const StyledLabel = styled(Label)`
-  position: absolute;
+  position: absolute !important;
   top: 1rem;
   right: 1rem;
-  font-size: 12px !important;
-  font-weight: bold !important;
-  color: ${SUB_COLOR} !important;
-  padding: .2rem .5rem !important;
-  border: 1.5px solid ${SUB_COLOR} !important;
-  border-radius: 3px !important;
-  background-color: #fff !important;
-  &:hover {
-    color: #fff !important;
-    background-color: ${SUB_COLOR} !important;
-  }
+  font-size: 11px !important;
+  color: #fff !important;
+  font-weight: 600 !important;
+  background-color: ${SUB_COLOR} !important;
 `
 
 const AcademyLabel = () => (
-  <StyledLabel basic>
-      교육 기관
-  </StyledLabel>
+  <StyledLabel as="div" tag>교육 기관</StyledLabel>
 )
 
 export default AcademyLabel
