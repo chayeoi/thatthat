eeecf344acb976ef4da6cb98494bfb1ceaa48dd5
bong kim <32095134+underbleu@users.noreply.github.com>
2018-03-07 06:54:58 +0900

    스타일링 - 카테고리탭 Sticky 적용

--- src/pages/CoursePage.js
+++ src/pages/CoursePage.js
@@ -1,28 +1,57 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Route, Switch } from 'react-router-dom'
+import { Rail, Sticky } from 'semantic-ui-react'
 import {
   MainMenuContainer,
   CategoryTabContainer,
   CourseListContainer,
 } from 'containers'
+import styled from 'styled-components'
 import { withUser } from 'hocs'
 
-const CoursePage = ({ userClass, location: { pathname } }) => (
-  <React.Fragment>
-    <MainMenuContainer />
-    <CategoryTabContainer pathName={pathname} />
-    <Switch>
-      <Route
-        exact
-        path="/courses"
-        render={({ match }) => <CourseListContainer userClass={userClass} match={match} />}
-      />
-      <Route
-        path="/courses/:category"
-        render={({ match }) => <CourseListContainer userClass={userClass} match={match} />}
-      />
-    </Switch>
-  </React.Fragment>
-)
+const WideRail = styled(Rail)`
+  width: 100% !important;
+  height: 3000px !important;
+`
+
+const LongGrid = styled.div`
+  height: 3000px !important;
+  background-color: yellow;
+`
+
+class CoursePage extends Component {
+  state = {}
+
+  handleContextRef = contextRef => this.setState({ contextRef })
+
+  render() {
+    const { contextRef } = this.state
+    const { userClass, location: { pathname } } = this.props
+
+    return (
+      <div ref={this.handleContextRef}>
+        <LongGrid>
+          <WideRail>
+            <MainMenuContainer />
+            <Sticky context={contextRef} style={{position: 'relative', zIndex: 9999}}>
+              <CategoryTabContainer pathName={pathname} />
+            </Sticky>
+            <Switch>
+              <Route
+                exact
+                path="/courses"
+                render={({ match }) => <CourseListContainer userClass={userClass} match={match} />}
+              />
+              <Route
+                path="/courses/:category"
+                render={({ match }) => <CourseListContainer userClass={userClass} match={match} />}
+              />
+            </Switch>
+          </WideRail>
+        </LongGrid>
+      </div>
+    )
+  }
+}
 
 export default withUser(CoursePage)
