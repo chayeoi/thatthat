64bc60cb278bf246a0f247d7c9d3d233b2b87117
bong kim <32095134+underbleu@users.noreply.github.com>
2018-03-08 01:26:36 +0900

    스타일링 - 마이페이지 좋아요 리스트 반응형 처리

--- src/components/MyLikeList.js
+++ src/components/MyLikeList.js
@@ -6,6 +6,12 @@ import * as color from '../constants/color'
 const Wrapper = styled.ul`
   padding: 1em;
 `
+
+const CenterBox = styled.div`
+  max-width: 768px;
+  margin: 0 auto !important;
+`
+
 const Total = styled.div`
   padding: 0 .5rem .7rem;
   & > em {
@@ -15,18 +21,20 @@ const Total = styled.div`
 
 const MyLikeList = ({ likes }) => (
   <Wrapper>
-    {likes.length ?
-      <Total>총 <em>{likes.length}개</em>의 관심 강의가 있습니다.</Total> :
-      <Total>관심 강의가 없습니다.</Total>
-    }
-    <ul>
-      {likes ?
-        likes.map(course => (
-          <CourseCard key={course.courseKey} course={course} />
-        ))
-        : null
+    <CenterBox>
+      {likes.length ?
+        <Total>총 <em>{likes.length}개</em>의 관심 강의가 있습니다.</Total> :
+        <Total>관심 강의가 없습니다.</Total>
       }
-    </ul>
+      <ul>
+        {likes ?
+          likes.map(course => (
+            <CourseCard key={course.courseKey} course={course} />
+          ))
+          : null
+        }
+      </ul>
+    </CenterBox>
   </Wrapper>
 )
 
