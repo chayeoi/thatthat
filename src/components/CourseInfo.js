46eadd33c328d4f08fe16c9f8f5dd346bb039eb6
bong kim <32095134+underbleu@users.noreply.github.com>
2018-03-07 17:50:44 +0900

    리팩토링 - 불필요 코드 제거

--- src/components/CourseInfo.js
+++ src/components/CourseInfo.js
@@ -1,11 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
-import * as color from '../constants/color'
 
 const Wrapper = styled.div`
   padding: 2em;
-  min-height: 100vh;
-  background-color: ${color.GRAY2};
 `
 
 const CourseInfo = ({ content }) => (
